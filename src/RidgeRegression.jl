785ecb0758193d70b7092b84272ff54b05a1a5ab
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-03-17 19:45:15 -0700

    initial implementation of feature grouping

--- src/RidgeRegression.jl
+++ src/RidgeRegression.jl
@@ -1,5 +1,12 @@
 module RidgeRegression
 
-greet() = print("Hello World!")
+import Base:reduce, rand
 
+include("groupedfeatures.jl")
+
+export GroupedFeatures,
+       group_idx,
+	   group_summary,
+	   group_expand,
+	   random_betas
 end # module
---
+++ src/groupedfeatures.jl
@@ -0,0 +1,50 @@
+
+struct GroupedFeatures
+    ps::Vector{Int}
+    p::Int
+    num_groups::Int
+end
+
+GroupedFeatures(ps) = GroupedFeatures(ps, sum(ps), length(ps))
+
+
+function group_idx(gr::GroupedFeatures, i::Integer)
+    starts = cumsum([1;gr.ps])[1:end-1]
+    ends = cumsum(gr.ps)
+    starts[i]:ends[i]
+end
+
+
+function group_summary(gr::GroupedFeatures, vec::AbstractVector, f)
+    ps = gr.ps
+    num_groups = gr.num_groups
+    starts = cumsum([1;ps])[1:end-1]
+    ends = cumsum(ps)
+    el = eltype(f(vec))
+    output = Vector{el}(undef, num_groups)
+    for g=1:num_groups
+        output[g] = f(vec[starts[g]:ends[g]])
+    end
+    output
+end
+
+function group_expand(gr::GroupedFeatures, vec::AbstractVector)
+    arr = zeros(eltype(vec), gr.p)
+    for i=1:gr.num_groups
+        arr[group_idx(gr, i)] .= vec[i]
+    end
+    arr
+end
+
+function group_expand(gr::GroupedFeatures, el::Number)
+    fill(el, gr.p)
+end
+
+function random_betas(gr::GroupedFeatures, αs)
+    ps = gr.ps
+    βs = zeros(eltype(αs), gr.p)
+    for i=1:gr.num_groups
+        βs[group_idx(gr, i)] .= randn(ps[i]) .* sqrt(αs[i]^2/ps[i])
+    end
+    βs
+end
