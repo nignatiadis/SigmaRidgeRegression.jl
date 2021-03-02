0e93344b53bfa9de00074de809c6e40626502b1f
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2021-03-01 21:25:05 -0800

    update simulation plots

--- reproduction_code/million_songs.jl
+++ reproduction_code/million_songs.jl
@@ -1,9 +1,9 @@
 using Pkg
 Pkg.activate(@__DIR__)
-using Pkg.Artifacts
 using CSV
 using DataFrames
 using StatsBase
+using RCall
 using SigmaRidgeRegression
 using LaTeXStrings
 using LinearAlgebra
@@ -14,92 +14,73 @@ using Plots
 using StatsPlots
 using PGFPlotsX
 
-#------------------------------------------------------------------
-# Code that generated the Artifact file Artifacts.toml
-#------------------------------------------------------------------
-#using ArtifactUtils
-#add_artifact!(
-#           "Artifacts.toml",
-#           "YearPredictionMSD",
-#           "https://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip",
-#           force=true,
-#       )
+msd_filepath = joinpath(@__DIR__, "dataset", "YearPredictionMSD.txt")
+msd = DataFrame(CSV.File(msd_filepath; header=false))
 
-
-#--------------------------------------------------------------------------------------
-# Command below may take a while since it will automatically download the Million Song
-# Dataset from the UCI repository (around 400 MB).
-#--------------------------------------------------------------------------------------
-
-msd_filepath = joinpath(artifact"YearPredictionMSD", "YearPredictionMSD.txt")
-msd = CSV.File(msd_filepath, header=false) |> DataFrame
-
-
-
-function feature_map(X) #; noisegroups=10, noisefeatures=50)
-	mean_features = X[:, 1:12]
-	var_features = X[:, 13:24]
-	sd_features = sqrt.(var_features)
-	#cv_features = sd_features ./ abs.(mean_features)
-	cov_features = X[:, 25:90]
-	cor_features = zeros(size(cov_features))
-	cnt = 0
-	for offset=1:11
-		for i=1:(12-offset)
-			cnt = cnt + 1
-			cor_features[:,cnt] = cov_features[:,cnt] ./ sqrt.(var_features[:,i] .* var_features[:,i+offset])
-		end
-	end
-	#noise_features = randn(size(X,1)#, noisegroups*noisefeatures )
-	grp = GroupedFeatures([12, 12, 66, 66]) #, fill(noisefeatures, noisegroups)))
-	(MLJ.table([mean_features sd_features cov_features cor_features]), grp)
+function feature_map(X)
+    mean_features = X[:, 1:12]
+    var_features = X[:, 13:24]
+    sd_features = sqrt.(var_features)
+    cov_features = X[:, 25:90]
+    cor_features = zeros(size(cov_features))
+    cnt = 0
+    for offset in 1:11
+        for i in 1:(12 - offset)
+            cnt = cnt + 1
+            cor_features[:, cnt] =
+                cov_features[:, cnt] ./
+                sqrt.(var_features[:, i] .* var_features[:, i + offset])
+        end
+    end
+    grp = GroupedFeatures([12, 12, 66, 66])
+    return (MLJ.table([mean_features sd_features cov_features cor_features]), grp)
 end
 
-
-
 Y = Float64.(msd[:, 1])
-X, groups = feature_map(Matrix(msd[:, 2:91]))
+X, groups = feature_map(Matrix(msd[:, 2:91]));
 
 train_idx = 1:463_715
-test_idx =  (1:51_630) .+ 463_715
+test_idx = (1:51_630) .+ 463_715
 
-single_ridge = SingleGroupRidgeRegressor(decomposition = :cholesky, groups=groups, λ=1.0, center=true, scale=true)
-loo_single_ridge = LooRidgeRegressor(ridge = deepcopy(single_ridge))
+loo_single_ridge = LooRidgeRegressor(;
+    ridge=SingleGroupRidgeRegressor(; groups=groups, center=true, scale=true)
+)
 
-sigma_ridge = SigmaRidgeRegressor(groups=groups, decomposition = :cholesky, σ=0.01, center=true, scale=true)
-loo_sigmaridge = LooRidgeRegressor(ridge=deepcopy(sigma_ridge), tuning=SigmaRidgeRegression.DefaultTuning(scale=:linear, param_min_ratio=0.001))
+loo_sigmaridge = LooSigmaRidgeRegressor(; groups=groups, center=true, scale=true)
 
-multi_ridge = MultiGroupRidgeRegressor(groups; decomposition = :cholesky, center=true, scale=true)
-loo_multi_ridge = LooRidgeRegressor(ridge = deepcopy(multi_ridge), rng=MersenneTwister(1))
+multi_ridge =
+    loo_multi_ridge = LooRidgeRegressor(;
+        ridge=MultiGroupRidgeRegressor(; groups=groups, center=true, scale=true),
+        rng=MersenneTwister(1),
+    )
 
-glasso = GroupLassoRegressor(groups=groups, decomposition = :cholesky, center=true, scale=true)
-holdout_glasso = TunedRidgeRegressor(ridge=deepcopy(glasso), resampling= Holdout(shuffle=true, rng=1), tuning=DefaultTuning(param_min_ratio=1e-5))
+holdout_glasso = TunedSeagull(; groups=groups, center=true, scale=true)
 
-
-ns_subsample = [200; 500; 1000]
+ns_subsample = [500; 1000; 5000]
 n_montecarlo = 20
-Random.seed!(1)
+Random.seed!(10)
 
+#an initial run to make sure everything is precompiled
 mse_array = Array{Float64}(undef, length(ns_subsample), n_montecarlo, 4)
 time_array = Array{Float64}(undef, length(ns_subsample), n_montecarlo, 4)
 λs_array = Array{Any}(undef, length(ns_subsample), n_montecarlo, 4)
+for n_montecarlo in [1; n_montecarlo]
 for (k, n_subsample) in enumerate(ns_subsample)
     for j in Base.OneTo(n_montecarlo)
-        train_idx_subsample = sample(train_idx, n_subsample, replace=false)
+        train_idx_subsample = sample(train_idx, n_subsample; replace=false)
         resampling_idx = [(train_idx_subsample, test_idx)]
-        for (i,mach) in enumerate([loo_sigmaridge, loo_single_ridge, loo_multi_ridge, holdout_glasso])
-            time_array[k,j,i] = @elapsed begin
+        for (i, mach) in
+            enumerate([loo_sigmaridge, loo_single_ridge, loo_multi_ridge, holdout_glasso])
+            time_array[k, j, i] = @elapsed begin
                 _mach = machine(mach, X, Y)
-                _eval = evaluate!(_mach, resampling=resampling_idx, measure=l2)
+                _eval = evaluate!(_mach; resampling=resampling_idx, measure=l2)
             end
-            mse_array[k,j,i] = _eval.measurement[1]
-            λs_array[k,j,i] = deepcopy(_mach.report.best_λs)
+            mse_array[k, j, i] = _eval.measurement[1]
+            λs_array[k, j, i] = deepcopy(_mach.report.best_λs)
         end
     end
 end
-
-
-
+end
 
 pgfplotsx()
 
@@ -111,125 +92,187 @@ push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\risk}[1]{\bm{R}(#1)}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage[bbgreekl]{mathbbol}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\sigmacv}{\bbsigma}")
 
+_orange = RGB{Float64}(0.933027, 0.665164, 0.198652)
+_orange2 = RGB{Float64}(0.85004, 0.540122, 0.136212)
 
-_orange =   RGB{Float64}(0.933027,0.665164,0.198652)
-_orange2 =  RGB{Float64}(0.85004,0.540122,0.136212)
-
-method_names = [L"\sigmacv-\textrm{Ridge}" L"\textrm{Single Ridge}" L"\textrm{Multi Ridge}" L"\textrm{Group Lasso}"]
+method_names =
+    [L"\sigmacv-\textrm{Ridge}" L"\textrm{Single Ridge}" L"\textrm{Multi Ridge}" L"\textrm{Group Lasso}"]
 
 _thickness_scaling = 1.8
 mse_plot = plot(
-dotplot(method_names, mse_array[1,:,:],
-    title=L"n\;=\;%$(ns_subsample[1])",
-    frame = :box, grid=nothing,
-    yguide = "Mean squared error",
-    label=nothing,
-    markerstrokecolor=_orange2,
-    markerstrokewidth=0.5,
-    thickness_scaling = _thickness_scaling,
-    ylim = (88,141),
-    color=_orange),
-dotplot(method_names, mse_array[2,:,:],
-    title=L"n\;=\;%$(ns_subsample[2])",
-    frame = :box, grid=nothing,
-    label=nothing,
-    markerstrokecolor=_orange2,
-    markerstrokewidth=0.5,
-    ylim = (88,141),
-    thickness_scaling = _thickness_scaling,
-    color=_orange),
-dotplot(method_names, mse_array[3,:,:],
-    title=L"n\;=\;%$(ns_subsample[3])",
-    frame = :box, grid=nothing,
-    label=nothing,
-    markerstrokecolor=_orange2,
-    markerstrokewidth=0.5,
-    thickness_scaling = _thickness_scaling,
-    ylim = (88,141),
-    color=_orange), size=(1650,400), layout=(1,3))
+    dotplot(
+        method_names,
+        mse_array[1, :, :];
+        title=L"n\;=\;%$(ns_subsample[1])",
+        frame=:box,
+        grid=nothing,
+        yguide="Mean squared error",
+        label=nothing,
+        markerstrokecolor=_orange2,
+        markerstrokewidth=0.5,
+        alpha=0.7,
+        thickness_scaling=_thickness_scaling,
+        ylim=(89, 110),
+        color=_orange,
+    ),
+    dotplot(
+        method_names,
+        mse_array[2, :, :];
+        title=L"n\;=\;%$(ns_subsample[2])",
+        frame=:box,
+        grid=nothing,
+        label=nothing,
+        markerstrokecolor=_orange2,
+        markerstrokewidth=0.5,
+        ylim=(89, 110),
+        alpha=0.7,
+        thickness_scaling=_thickness_scaling,
+        color=_orange,
+    ),
+    dotplot(
+        method_names,
+        mse_array[3, :, :];
+        title=L"n\;=\;%$(ns_subsample[3])",
+        frame=:box,
+        grid=nothing,
+        label=nothing,
+        markerstrokecolor=_orange2,
+        markerstrokewidth=0.5,
+        thickness_scaling=_thickness_scaling,
+        alpha=0.7,
+        ylim=(89, 110),
+        color=_orange,
+    );
+    size=(1650, 400),
+    layout=(1, 3),
+)
+
+
 
 
 savefig(mse_plot, "one_million_songs_mse.pdf")
 
 time_plot = plot(
-        dotplot(method_names, time_array[1,:,:],
-            title=L"n\;=\;%$(ns_subsample[1])",
-            frame = :box, grid=nothing,
-            yguide = "Time (seconds)",
-            label=nothing,
-            markerstrokecolor=_orange2,
-            markerstrokewidth=0.5,
-            ylim = (-0.5,10.5),
-            thickness_scaling = _thickness_scaling,
-            color=_orange),
-        dotplot(method_names, time_array[2,:,:],
-            title=L"n\;=\;%$(ns_subsample[2])",
-            frame = :box, grid=nothing,
-            label=nothing,
-            markerstrokecolor=_orange2,
-            markerstrokewidth=0.5,
-            thickness_scaling = _thickness_scaling,
-            ylim = (-0.5,10.5),
-            color=_orange),
-        dotplot(method_names, time_array[3,:,:],
-            title=L"n\;=\;%$(ns_subsample[3])",
-            frame = :box, grid=nothing,
-            label=nothing,
-            markerstrokecolor=_orange2,
-            markerstrokewidth=0.5,
-            thickness_scaling = _thickness_scaling,
-            ylim = (-0.5,10.5),
-            color=_orange), size=(1650,400), layout=(1,3))
+    dotplot(
+        method_names,
+        time_array[1, :, :];
+        title=L"n\;=\;%$(ns_subsample[1])",
+        frame=:box,
+        grid=nothing,
+        yguide="Time (seconds)",
+        label=nothing,
+        markerstrokecolor=_orange2,
+        alpha=0.7,
+        markerstrokewidth=0.5,
+        yscale=:log10,
+        ylim = (0.1, 100),
+        yticks = ([0.1, 1,10,100], ["0.1","1","10","100"]),
+        #ylim=(-0.5, 58.0),
+        thickness_scaling=_thickness_scaling,
+        color=_orange,
+    ),
+    dotplot(
+        method_names,
+        time_array[2, :, :];
+        title=L"n\;=\;%$(ns_subsample[2])",
+        frame=:box,
+        grid=nothing,
+        label=nothing,
+        alpha=0.7,
+        markerstrokecolor=_orange2,
+        markerstrokewidth=0.5,
+        thickness_scaling=_thickness_scaling,
+        yscale=:log10,
+        ylim = (0.1, 100),
+        yticks = ([0.1, 1,10,100], ["0.1","1","10","100"]),
+        #ylim=(-0.5, 58.0),
+        color=_orange,
+    ),
+    dotplot(
+        method_names,
+        time_array[3, :, :];
+        title=L"n\;=\;%$(ns_subsample[3])",
+        frame=:box,
+        grid=nothing,
+        label=nothing,
+        alpha=0.7,
+        markerstrokecolor=_orange2,
+        markerstrokewidth=0.5,
+        thickness_scaling=_thickness_scaling,
+        yscale=:log10,
+        ylim = (0.1, 100),
+        yticks = ([0.1, 1,10,100], ["0.1","1","10","100"]),
+        #ylim=(-0.5, 58.0),
+        color=_orange,
+    );
+    size=(1650, 400),
+    layout=(1, 3),
+)
 
 savefig(time_plot, "one_million_songs_time.pdf")
 
-
 _trunc = 20
 
-λs_mean = min.(getindex.(λs_array,1), 20)
-λs_std = min.(getindex.(λs_array,2), 20)
-λs_cov = min.(getindex.(λs_array,3), 20)
-λs_cor = min.(getindex.(λs_array,4), 20)
+λs_mean = min.(getindex.(λs_array, 1), 20)
+λs_std = min.(getindex.(λs_array, 2), 20)
+λs_cov = min.(getindex.(λs_array, 3), 20)
+λs_cor = min.(getindex.(λs_array, 4), 20)
 
-λs_sigma_ridge = [λs_mean[1,:,1] λs_std[1,:,1] λs_cov[1,:,1] λs_cor[1,:,1]]
+λs_sigma_ridge = [λs_mean[1, :, 1] λs_std[1, :, 1] λs_cov[1, :, 1] λs_cor[1, :, 1]]
 
-feature_names = ["mean" "std"  "cov" "cor"]
-λs_names = [L"\hat{\lambda}_{\textrm{%$n}}"  for n in feature_names]
+feature_names = ["mean" "std" "cov" "cor"]
+λs_names = [L"\hat{\lambda}_{\textrm{%$n}}" for n in feature_names]
 
-
-λ_plot_params = (frame = :box, grid=nothing,
+λ_plot_params = (;
+    frame=:box,
+    grid=nothing,
     label="",
     markerstrokecolor=:purple,
     markerstrokewidth=0.5,
-    markercolor= RGB{Float64}(205/256,153/256,255/256),
-    thickness_scaling = 1.7,
-    ylim = (-0.9,_trunc + 0.8))
-
-#λ_yguide=L"\min\{\widehat{\lambda},20\}",
-
-method_names_short = [L"\sigmacv-\textrm{Ridge}" L"\textrm{Single}" L"\textrm{Multi}" L"\textrm{GLasso}"]
+    markercolor=RGB{Float64}(205 / 256, 153 / 256, 255 / 256),
+    thickness_scaling=1.7,
+    ylim=(-0.9, _trunc + 0.8),
+)
 
 
-plot(
-dotplot(method_names_short, λs_mean[1,:,:]; yguide=L"\min\{\widehat{\lambda},20\}", title=λs_names[1]),
-dotplot(method_names_short, λs_std[1,:,:], title=λs_names[2]),
-dotplot(method_names_short, λs_cov[1,:,:], title=λs_names[3]),
-dotplot(method_names_short, λs_cor[1,:,:], title=λs_names[4]),
-size=(1500,270), layout=(1,4); λ_plot_params...)
-savefig("one_million_song_lambdas_n200.pdf")
+method_names_short =
+    [L"\sigmacv-\textrm{Ridge}" L"\textrm{Single}" L"\textrm{Multi}" L"\textrm{GLasso}"]
 
 plot(
-dotplot(method_names_short, λs_mean[2,:,:]; yguide=L"\min\{\widehat{\lambda},20\}"),
-dotplot(method_names_short, λs_std[2,:,:]),
-dotplot(method_names_short, λs_cov[2,:,:]),
-dotplot(method_names_short, λs_cor[2,:,:]),
-size=(1500,260), layout=(1,4); λ_plot_params...)
+    dotplot(
+        method_names_short,
+        λs_mean[1, :, :];
+        yguide=L"\min\{\widehat{\lambda},20\}",
+        title=λs_names[1],
+    ),
+    dotplot(method_names_short, λs_std[1, :, :]; title=λs_names[2]),
+    dotplot(method_names_short, λs_cov[1, :, :]; title=λs_names[3]),
+    dotplot(method_names_short, λs_cor[1, :, :]; title=λs_names[4]);
+    size=(1500, 270),
+    layout=(1, 4),
+    λ_plot_params...
+)
+savefig("one_million_song_lambdas_n500.pdf")
+
+plot(
+    dotplot(method_names_short, λs_mean[2, :, :]; yguide=L"\min\{\widehat{\lambda},20\}"),
+    dotplot(method_names_short, λs_std[2, :, :]),
+    dotplot(method_names_short, λs_cov[2, :, :]),
+    dotplot(method_names_short, λs_cor[2, :, :]);
+    size=(1500, 260),
+    layout=(1, 4),
+    λ_plot_params...
+)
 savefig("one_million_song_lambdas_n1000.pdf")
 
 plot(
-dotplot(method_names_short, λs_mean[3,:,:]; yguide=L"\min\{\widehat{\lambda},20\}"),
-dotplot(method_names_short, λs_std[3,:,:]),
-dotplot(method_names_short, λs_cov[3,:,:]),
-dotplot(method_names_short, λs_cor[3,:,:]),
-size=(1500,260), layout=(1,4); λ_plot_params...)
+    dotplot(method_names_short, λs_mean[3, :, :]; yguide=L"\min\{\widehat{\lambda},20\}"),
+    dotplot(method_names_short, λs_std[3, :, :]),
+    dotplot(method_names_short, λs_cov[3, :, :]),
+    dotplot(method_names_short, λs_cor[3, :, :]);
+    size=(1500, 260),
+    layout=(1, 4),
+    λ_plot_params...,
+)
+
 savefig("one_million_song_lambdas_n5000.pdf")
--- reproduction_code/simulations_plots.jl
+++ reproduction_code/simulations_plots.jl
@@ -11,6 +11,7 @@ using PGFPlotsX
 
 pgfplotsx()
 
+empty!(PGFPlotsX.CUSTOM_PREAMBLE)
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage{amsmath}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage{amssymb}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage{bm}")
@@ -20,75 +21,120 @@ push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\sigmacv}{\bbsigma}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\bSigma}{\bm{\Sigma}}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\bw}{\bm{w}}")
 
+sim_path = joinpath(@__DIR__, "simulation_results")
 
-
-
-
-all_files = load.(readdir("simulation_results"))
+all_files = FileIO.load.(joinpath.(sim_path, readdir(sim_path)))
 all_files = load("simulation_results/loaded_files.jld2")["loaded_files"]
-all_tuples = vcat([x["res"] for x in all_files]...)
+all_tuples = vcat((x["res"] for x in all_files)...)
 
 df = DataFrame(all_tuples)
 df.n = getfield.(df.sim, :ntrain)
 
+summary_f = x -> mean(x) .- 25 #25 is noise variance
 
-summary_f = x-> median(x) .- 25 #25 is noise variance
-
-gdf = groupby(df, [:cov,:n ,:K]) |>
-      df -> combine(df, :mse_sigma =>  summary_f => :mse_sigma,
-                        :mse_single => summary_f  => :mse_single,
-                        :mse_multi => summary_f  => :mse_multi,
-                        :mse_glasso => summary_f  => :mse_glasso,
-                        :mse_bayes => summary_f  => :mse_bayes,
-                        nrow) |>
-      df -> groupby(df, [:n,:cov])
-
-
-f_tbl_norm(i) = [gdf[i].mse_sigma  gdf[i].mse_single gdf[i].mse_multi gdf[i].mse_glasso] ./ gdf[i].mse_bayes
-f_tbl(i) = [gdf[i].mse_single gdf[i].mse_glasso gdf[i].mse_multi gdf[i].mse_sigma  gdf[i].mse_bayes]
+gdf = df ->
+    combine(
+        df,
+        :mse_sigma => summary_f => :mse_sigma,
+        :mse_single => summary_f => :mse_single,
+        :mse_multi => summary_f => :mse_multi,
+        :mse_glasso => summary_f => :mse_glasso,
+        :mse_bayes => summary_f => :mse_bayes,
+        nrow,
+    ) |> df -> groupby(df, [:cov, :n])(groupby(df, [:cov, :n, :K]))
 
 
+function f_tbl(i)
+    return [gdf[i].mse_single gdf[i].mse_glasso gdf[i].mse_multi gdf[i].mse_sigma gdf[i].mse_bayes]
+end
 
-_cols =  [:steelblue :green :orange :purple :grey]
-_markers = [:utriangle :dtriangle :diamond   :pentagon  :circle]
-_labels = ["Single Ridge" "Group Lasso" "Multi Ridge" L"\sigmacv\textrm{-Ridge}"  "Bayes"]
-_linestyles =[:dot :dashdot :dashdotdot :solid :dash]
+_cols = [:steelblue :green :orange :purple :grey]
+_markers = [:utriangle :dtriangle :diamond :pentagon :circle]
+_labels = ["Single Ridge" "Group Lasso" "Multi Ridge" L"\sigmacv\textrm{-Ridge}" "Bayes"]
+_linestyles = [:dot :dashdot :dashdotdot :solid :dash]
 
-plot_params = (frame = :box, grid=nothing,
-    color = _cols,
-    background_color_legend = :transparent,
-    foreground_color_legend = :transparent,
-    thickness_scaling = 2.3,
-    markershape = _markers,
-    ylabel = L"\risk{\widehat{\bw}} - \sigma^2",
-    linestyle = _linestyles,
+plot_params = (
+    frame=:box,
+    grid=nothing,
+    color=_cols,
+    background_color_legend=:transparent,
+    foreground_color_legend=:transparent,
+    thickness_scaling=2.3,
+    markershape=_markers,
+    ylabel=L"\mathbb{E}[\risk{\widehat{\bw}}] - \sigma^2",
+    linestyle=_linestyles,
     xlabel=L"K",
-    xscale = :log2,
+    xscale=:log2,
     markeralpha=0.6,
-    size= (550, 440))
+    size=(550, 440),
+)
 
-pl1= plot(gdf[1].K,  f_tbl(1); label=_labels, legend=:topleft, title=L"\bSigma=\textrm{AR}(0.8),\;n=p/2", ylim=(0,650), plot_params...)
-pl2= plot(gdf[2].K,  f_tbl(2); label=nothing, title=L"\bSigma=\textrm{AR}(0.8),\;n=p", ylim=(0,220), plot_params...)
-pl3= plot(gdf[3].K,  f_tbl(3); label=nothing, title=L"\bSigma=\textrm{AR}(0.8),\;n=2p", ylim=(0,45), plot_params...)
+idx1 = (cov=AR1Design{Int64}(800, 0.8), n=400)
+pl1 = plot(
+    gdf[idx1].K,
+    f_tbl(idx1);
+    label=_labels,
+    legend=:topleft,
+    title=L"\bSigma=\textrm{AR}(0.8),\;n=p/2",
+    ylim=(0, 795),
+    plot_params...,
+)
 
-pl4= plot(gdf[4].K,  f_tbl(4); label=nothing, title=L"\bSigma=I,\; n=p/2", legend=:topleft, ylim=(0,650), plot_params... )
-pl5= plot(gdf[5].K,  f_tbl(5); label=nothing, title=L"\bSigma=I,\; n=p", ylim=(0,220), plot_params... )
-pl6= plot(gdf[6].K,  f_tbl(6); label=nothing, title=L"\bSigma=I,\; n=2p", ylim=(0,45), plot_params... )
+idx2 = (cov=AR1Design{Int64}(800, 0.8), n=800)
+pl2 = plot(
+    gdf[idx2].K,
+    f_tbl(idx2);
+    label=nothing,
+    title=L"\bSigma=\textrm{AR}(0.8),\;n=p",
+    ylim=(0, 285),
+    plot_params...,
+)
+
+idx3 = (cov=AR1Design{Int64}(800, 0.8), n=1600)
+pl3 = plot(
+    gdf[idx3].K,
+    f_tbl(idx3);
+    label=nothing,
+    title=L"\bSigma=\textrm{AR}(0.8),\;n=p",
+    ylim=(0, 55),
+    plot_params...,
+)
+
+idx4 = (cov=IdentityCovarianceDesign{Int64}(800), n=400)
+pl4 = plot(
+    gdf[idx4].K,
+    f_tbl(idx4);
+    label=nothing,
+    title=L"\bSigma=I,\; n=p/2",
+    legend=:topleft,
+    ylim=(0, 795),
+    plot_params...,
+)
+
+idx5 = (cov=IdentityCovarianceDesign{Int64}(800), n=800)
+pl5 = plot(
+    gdf[idx5].K,
+    f_tbl(idx5);
+    label=nothing,
+    title=L"\bSigma=I,\; n=p",
+    ylim=(0, 285),
+    plot_params...,
+)
+
+idx6 = (cov=IdentityCovarianceDesign{Int64}(800), n=1600)
+pl6 = plot(
+    gdf[6].K,
+    f_tbl(6);
+    label=nothing,
+    title=L"\bSigma=I,\; n=2p",
+    ylim=(0, 55),
+    plot_params...,
+)
 
 savefig(pl1, "simulations_ar_phalf.tikz")
 savefig(pl2, "simulations_ar_p.tikz")
 savefig(pl3, "simulations_ar_ptwice.tikz")
 
-
 savefig(pl4, "simulations_id_phalf.tikz")
 savefig(pl5, "simulations_id_p.tikz")
 savefig(pl6, "simulations_id_ptwice.tikz")
-
-
-pl_ar= plot(pl1,pl2,pl3, size=(1150,280), layout=(1,3))
-savefig(pl_ar, "simulations_ar.tikz")
-
-pl_id= plot(pl4,pl5,pl6, size=(1150,280), layout=(1,3))
-
-pl = plot(pl4,pl5,pl6,pl1,pl2,pl3, size=(1200,800), layout=(2,3))
-savefig(pl_ar, "simulations_identity.tikz")
